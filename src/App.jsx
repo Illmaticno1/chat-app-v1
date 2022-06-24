938757bd58f61cc167ec3920d44a52382613d2e9
Illmaticno1 <Demetrius.robinson@icloud.com>
2022-06-24 00:08:41 -0400

    added assets

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Cookies from 'universal-cookie';
 
 import { ChannelListContainer, ChannelContainer } from './components';
 
+import './App.css';
+
 const apiKey = '35c7d8dbsmzv';
 
 const client = StreamChat.getInstance(apiKey);
